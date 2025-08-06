f59b460558ff5891e61556feabd9843c36c7d686
Tomas Ciernik <tomsky@email.com>
2025-08-06 15:37:51 +0100

    added docker file - couldnt test it, cant pull any packages cos certs

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
@@ -28,6 +26,6 @@ def trigger_scrape(request: ScrapeRequest):
 
     return JSONResponse(content=success)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
